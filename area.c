5f5fb8946b4c20e0e2a39cf02eac146493297d95
kunalyadav1110 <117518867+kunalyadav1110@users.noreply.github.com>
2023-07-27 21:53:24 +0530

    Add files via upload

---
+++ area.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	float radius, area;
+	printf("enter the radius of the sphere");
+	scanf("%f,&radius");
+	area=4*3.14*radius*radius;
+	printf("area of circle is %2f, area");
+	return 0;
+}
